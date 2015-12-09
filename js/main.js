fc41caea6144744186471a9f4a8da614f1179d21
Radarn <admin@Arthurs-dator.local>
2015-12-09 22:42:02 +0100

    Added done button to To Do section. Also started adding elements in the js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//1. Get Buttons and Input
+var addTaskButton = document.querySelector(".AddTask .btn");
+var doneButton = document.querySelector(".ToDoTasks .done");
+var removeButton = document.querySelector(".ToDoTasks .remove");
+var editButton = document.querySelector(".ToDoTasks .edit");
+var finishButton = document.querySelector(".FinishedTasks .btn");
+var input = document.querySelector("input");
