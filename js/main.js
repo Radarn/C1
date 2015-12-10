6dfc664c82d75b021f35ccc85302fb0e2bd019f1
Radarn <admin@Arthurs-dator.local>
2015-12-10 14:29:30 +0100

    Added label as element in html and almost finished addTask function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,54 @@
 //1. Get Buttons and Input
-var addTaskButton = document.querySelector(".AddTask .btn");
-var doneButton = document.querySelector(".ToDoTasks .done");
-var removeButton = document.querySelector(".ToDoTasks .remove");
-var editButton = document.querySelector(".ToDoTasks .edit");
-var finishButton = document.querySelector(".FinishedTasks .btn");
-var input = document.querySelector("input");
+var buttons = document.querySelectorAll("button");
+var input = document.querySelector(".AddTask input");
+var toDoList = document.querySelector(".ToDoTasks ul");
+
+
+for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", taskManager);
+    }
+
+function taskManager() {
+	if(this.id === "addButton") {
+		addTask();
+
+	} else if(this.id === "removeButton") {
+		removeTask();
+	} else if (this.id === "editButton") {
+		editTask();
+	} else {
+		finishedTask();
+	}
+}
+function addTask() {
+	
+	var li = document.createElement("li");
+	var label = document.createElement("label");
+	var checkbox = document.createElement("input");
+	var textField = document.createElement("input");
+
+	textField.type = "text";
+	checkbox.type = "checkbox";
+    label.innerHTML = input.value;
+
+   	li.appendChild(label);
+    li.appendChild(textField);
+    li.appendChild(checkbox);
+    toDoList.appendChild(li);
+}
+
+function removeTask() {
+
+}
+
+function editTask() {
+
+}
+
+function finishedTask() {
+
+}
+
+
+
+
