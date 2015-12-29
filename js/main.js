9a44a72383322c3a07480805817eda3d4dfedb61
Radarn <admin@Arthurs-dator.local>
2015-12-29 07:39:28 +0100

    m

--- js/main.js
+++ js/main.js
@@ -49,17 +49,16 @@ function addTask() {
 	var checkbox = document.createElement("input");
 	var textField = document.createElement("input");
 
+
 	textField.type = "text";
 	checkbox.type = "checkbox";
 
 	checkbox.addEventListener("click", checkboxStatus);
-	label.addEventListener("dblclick", editMode);
 
 	checkbox.id = "cb_" + totalItems;
 	label.id = "item_" + totalItems;
 	textField.id = "edit_" + totalItems;
 	textField.className = "closed";
-	label.className = "closed";
 
 	textField.style.display = "none";
 
@@ -84,28 +83,9 @@ function removeTask() {
 }
 
 function editMode() {
-	alert("hej")
-	var id = this.id.replace("item_", "");
-	var label = document.getElementById("item_" + id);
-	var editText = document.getElementById("edit_" + id);
-	var checkbox = document.getElementById("cb_" + id);
-
-	if (editText.style.display === "none") {
-		editText.value = label.innerHTML;
-		editText.style.display = "";
-		checkbox.style.display = "none";
-	} else {
-		label.innerHTML = editText.value;
-		editText.style.display = "none";
-		checkbox.style.display = "";
-	}
-
 	
-}
 
-
-
-	/*var listItems = document.querySelectorAll('input');
+	var listItems = document.querySelectorAll('input');
 
 	for (var i = 0; i < listItems.length; i++ ) {
        if (listItems[i].type === 'checkbox') {
@@ -142,9 +122,8 @@ function editMode() {
 			listItemCheckboxFalse[a].checked = false;
 			
 		} 
-	}	*/
-
-
+	}	
+}
 	
 
 
@@ -166,6 +145,8 @@ function doneTasks() {
 	for (var i = 0; i < listItems.length; i++ ) {
        if (listItems[i].type === 'checkbox') {
            if (listItems[i].checked === true) {
+           		var x = listItems[i].parentElement.getElementsByClassName('checked');
+				var itemNumber = x[0].id.replace("item_", "");
 				completedList.appendChild((listItems[i].parentElement));
 				listItems[i].checked = false;
 			 
